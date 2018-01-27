57137feb6d638f91f2287f95413bb29ee70645a5
aaknight623 <alexander.a.knight@gmail.com>
2018-01-27 16:02:30 -0500

    Module3 - 1

---
+++ Module3.R
@@ -0,0 +1,12 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+ABC_political poll results <- c(4, 62, 51, 21, 2, 14, 15)
+NCB_political poll results <- c(12, 75, 43, 19, 1, 21, 19)
+polls <- cbind(Name, ABC_political poll results, NCB political poll_results)
+polls
+polls.df <- data.frame(Name, ABC_political poll results, NCB_political poll results)
+polls.df
+mean(polls.df)
+mean(polls.df[,2:3])
+as.matrix(polls.df)
+mean(polls)
+mean(as.data.frame(polls))
