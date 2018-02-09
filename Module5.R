d66cbf4b81ee5e5ae5e3aff666be82c98b7b1dfa
aaknight623 <alexander.a.knight@gmail.com>
2018-02-09 17:41:08 -0500

    Module 5-1

---
+++ Module5.R
@@ -0,0 +1,8 @@
+A <- matrix(1:100, nrow=6)        #Create Matrix A
+B <- matrix(1:1000, nrow=6)       #Create Matrix B
+A
+det(A)                            #Get Determinant of A
+solve(A)                          #Solve for Inverse of A
+B
+det(B)                            #Get Determinant of B
+solve(B)                          #Solve for Inverse of B
