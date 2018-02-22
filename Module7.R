24b62b633b827732929423444263d703e4f25f07
aaknight623 <35351556+aaknight623@users.noreply.github.com>
2018-02-21 23:44:44 -0500

    Delete Module7.R

--- Module7.R
+++
@@ -1,8 +0,0 @@
-lakes <- read.csv("Lakes.csv", TRUE, ",")
-plot(lakes)
-print(lakes)
-summary(lakes)
-predict(lakes)
-class(lakes) = "Lakes"
-lakes
-lakes < setClass("Lakes", slots=c(Lakes="character", elevation="numeric", area="numeric"))
